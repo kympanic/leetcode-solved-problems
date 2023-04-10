031478e645d52da2637083bad5180502eec1069f
Daniel Yoo <dacapo0509@gmail.com>
2023-04-09 20:16:25 -0700

    Time: 66 ms (37.76%), Space: 42.6 MB (36.97%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,11 +2,11 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
+const runningSum = (nums)=> {
     let newArray = [];
-    newArray[0] = nums[0];
-    for(let i = 1; i < nums.length; i++){
-        newArray[i] = nums[i] + newArray[i-1];
+    newArray[0] = nums[0]
+    for(let i = 1; i < nums.length;i++){
+        newArray[i] = nums[i] + newArray[i-1]
     }
-    return newArray;
-};
+    return newArray
+}
