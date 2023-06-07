b3b154b5d3ab2e16f042cb5e4b43dfc101a675c2
Daniel Yoo <dacapo0509@gmail.com>
2023-06-06 19:35:43 -0700

    Time: 59 ms (52.37%), Space: 42.3 MB (31.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -7,6 +7,5 @@ const s = "   fly me   to.   the moon  "
 var lengthOfLastWord = function(s) {
     //trim replaces the white space on both ends
     const array = s.trim().split(" ")
-    console.log(array)
     return array[array.length-1].length
 };
