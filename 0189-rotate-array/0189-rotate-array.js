8a9c8b093e3a5a56697e91076c314b3e4e556a7c
Daniel Yoo <dacapo0509@gmail.com>
2023-06-10 14:41:46 -0700

    Time: 99 ms (49.40%), Space: 51.3 MB (83.49%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -3,7 +3,9 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
+
+
+const rotate = (nums, k) => {
     k %= nums.length
     let endOfArray = nums.splice(nums.length - k)
     nums = nums.splice(0,0, ...endOfArray)
