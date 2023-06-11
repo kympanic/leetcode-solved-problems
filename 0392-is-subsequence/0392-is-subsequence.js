8065da119f55a63f2f8bb7b7920f74d774b60b99
Daniel Yoo <dacapo0509@gmail.com>
2023-06-10 18:27:14 -0700

    Time: 56 ms (79.56%), Space: 42.5 MB (17.93%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,13 +4,14 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    if(s.length > t.length) return false;
-    
+    if(s.length > t.length) return false
+    // at the end if subsequence and t_length are equal, than it is true, otherwise false
     const t_length = t.length;
     let subsequence = 0;
-    for(let i = 0;i < t_length; i++){
+    for(let i = 0; i < t_length; i++){
+        // increase subsequence everytime the letter at index matches index at string t
         if(s[subsequence] === t[i]){
-            subsequence++;
+            subsequence++
         }
     }
     return subsequence === s.length;
