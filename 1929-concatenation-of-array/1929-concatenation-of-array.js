6d5c8333cdc4d20c44fc1e012c7ff133ffcb242f
Daniel Yoo <dacapo0509@gmail.com>
2023-06-12 16:10:44 -0700

    Time: 70 ms (85.06%), Space: 46.5 MB (13.17%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,10 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let newArray = [];
-    for(let i =0; i < nums.length; i ++){
-        newArray[i] = nums[i];
-        newArray[i + nums.length] = nums[i]
-    }
-    return newArray;
+    return [...nums,...nums]
 };
