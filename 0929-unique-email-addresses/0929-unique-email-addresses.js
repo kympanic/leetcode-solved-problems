4d55c2dd2598d71a3b726b90f6336f0fc8643e4b
Daniel Yoo <dacapo0509@gmail.com>
2023-06-12 16:30:20 -0700

    Time: 71 ms (83.43%), Space: 46.1 MB (90.53%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.js
+++ 0929-unique-email-addresses/0929-unique-email-addresses.js
@@ -2,39 +2,51 @@
  * @param {string[]} emails
  * @return {number}
  */
-var numUniqueEmails = function(emails) {
-  //use hash set to store all the unique emails
+// var numUniqueEmails = function(emails) {
+//   //use hash set to store all the unique emails
     
-  let uniqueEmails = new Set();
+//   let uniqueEmails = new Set();
     
-  emails.forEach((email) => {
-        let cleanMail = [];
+//   emails.forEach((email) => {
+//         let cleanMail = [];
 
-        // iterate over each character in email
-        for (let i = 0; i < email.length; ++i) {
-            let currChar = email.charAt(i);
+//         // iterate over each character in email
+//         for (let i = 0; i < email.length; ++i) {
+//             let currChar = email.charAt(i);
 
-            // stop adding characters to localName
-            if (currChar === "+" || currChar === "@") break;
+//             // stop adding characters to localName
+//             if (currChar === "+" || currChar === "@") break;
 
-            // add this character if not '.'
-            if (currChar !== ".") cleanMail.push(currChar);
-        }
+//             // add this character if not '.'
+//             if (currChar !== ".") cleanMail.push(currChar);
+//         }
 
-        // compute domain name (substring from end to '@')
-        let domainName = [];
+//         // compute domain name (substring from end to '@')
+//         let domainName = [];
 
-        for (i = email.length - 1; i >= 0; --i) {
-            let currChar = email.charAt(i);
-            domainName.push(currChar);
-            if (currChar == "@") break;
-        }
+//         for (i = email.length - 1; i >= 0; --i) {
+//             let currChar = email.charAt(i);
+//             domainName.push(currChar);
+//             if (currChar == "@") break;
+//         }
 
-        domainName = domainName.reverse().join("");
-        cleanMail = cleanMail.join("");
+//         domainName = domainName.reverse().join("");
+//         cleanMail = cleanMail.join("");
 
-        uniqueEmails.add(cleanMail + domainName);
-    });
+//         uniqueEmails.add(cleanMail + domainName);
+//     });
 
+//     return uniqueEmails.size
+// };
+
+let numUniqueEmails = function (emails){
+    let uniqueEmails = new Set();
+    
+    emails.forEach((email)=> {
+        let parts = email.split("@")
+        let local = parts[0].split("+");
+        uniqueEmails.add(local[0].replaceAll(".","")+"@" + parts[1])
+    })
     return uniqueEmails.size
-};
+    
+}
