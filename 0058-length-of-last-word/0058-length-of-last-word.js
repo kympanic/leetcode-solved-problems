9179dcab9e23e132e853bd1285cfe61869efa749
Daniel Yoo <dacapo0509@gmail.com>
2023-06-12 17:05:02 -0700

    Time: 56 ms (69.19%), Space: 42.3 MB (30.70%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -5,3 +5,19 @@
 var lengthOfLastWord = function(s) {
     return s.trim().split(" ").pop().length
 };
+
+
+// var lengthOfLastWord = function(s) {
+//     let i = s.length - 1
+//     let count = 0
+//     while(i >= 0){
+//         if(s[i] === " " && count > 0){
+//             return count
+//         }else if(s[i] !== " ")
+//             count++
+//         i--
+//     }
+//     return count
+    
+// };
+    
