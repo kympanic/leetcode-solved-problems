f3a38f0aa3e90059e2a858f2167e21017384d25d
Daniel Yoo <dacapo0509@gmail.com>
2023-06-12 17:04:18 -0700

    Time: 46 ms (96.96%), Space: 42.5 MB (14.26%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,20 +2,6 @@
  * @param {string} s
  * @return {number}
  */
-
-
 var lengthOfLastWord = function(s) {
-    let i = s.length - 1
-    let count = 0
-    while(i >= 0){
-        if(s[i] === " " && count > 0){
-            return count
-        }else if(s[i] !== " ")
-            count++
-        i--
-    }
-    return count
-    
+    return s.trim().split(" ").pop().length
 };
-    
-   
