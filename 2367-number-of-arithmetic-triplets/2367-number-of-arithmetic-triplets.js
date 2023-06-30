2afc6e27981a56e247dab6df5be1bcd2a4e95372
Daniel Yoo <dacapo0509@gmail.com>
2023-06-29 21:44:36 -0700

    Time: 52 ms (95.44%), Space: 41.9 MB (79.75%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+    let count = 0;
+    for(let num of nums){
+        if(nums.includes(num+diff) && nums.includes(num+diff*2)){
+            count++
+        }
+    }
+    return count;
+};
+    
+    
