21d45c065edb73bce550c1fddf88df7029fc0843
Daniel Yoo <dacapo0509@gmail.com>
2023-07-05 20:20:48 -0700

    Time: 52 ms (89.45%), Space: 41.8 MB (80.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,18 +5,19 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    let nums1Copy = nums1.slice(0,m)
-    let p1= 0;
-    let p2= 0;
-    // Compare elements from nums1Copy and nums2 and write the smallest to nums1.
-  for (let p = 0; p < m + n; p++) {
-    // We also need to ensure that p1 and p2 aren't over the boundaries
-    // of their respective arrays.
-    if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
-      nums1[p] = nums1Copy[p1++];
+const merge = (nums1, m, nums2, n) => {
+  let left = m - 1
+  let right = n - 1
+
+  for (let i = nums1.length - 1; i >= 0; i--) {
+    if (right < 0) {
+      break
+    }
+
+    if (left >= 0 && nums1[left] > nums2[right]) {
+      nums1[i] = nums1[left--]
     } else {
-      nums1[p] = nums2[p2++];
+      nums1[i] = nums2[right--]
     }
   }
 }
