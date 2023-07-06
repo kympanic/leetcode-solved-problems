ffb695ff841d4153b06b608037d58c94648bcbb7
Daniel Yoo <dacapo0509@gmail.com>
2023-07-05 20:17:40 -0700

    Time: 51 ms (92.66%), Space: 41.7 MB (80.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,16 +5,18 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-
-function compareNumbers(a, b) {
-  return a - b;
-}
-
 var merge = function(nums1, m, nums2, n) {
-    for(let i = 0; i < n; i++){
-        nums1[m+i] = nums2[i]
+    let nums1Copy = nums1.slice(0,m)
+    let p1= 0;
+    let p2= 0;
+    // Compare elements from nums1Copy and nums2 and write the smallest to nums1.
+  for (let p = 0; p < m + n; p++) {
+    // We also need to ensure that p1 and p2 aren't over the boundaries
+    // of their respective arrays.
+    if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+      nums1[p] = nums1Copy[p1++];
+    } else {
+      nums1[p] = nums2[p2++];
     }
-    nums1.sort(compareNumbers)
-    return
-};
-//O(N) Time and O(1)Space
+  }
+}
