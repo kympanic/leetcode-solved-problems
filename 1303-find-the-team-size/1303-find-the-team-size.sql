db3f56315a6f5062426240b18578a5a54ecd8494
Daniel Yoo <dacapo0509@gmail.com>
2023-07-23 12:32:01 -0700

    Time: 543 ms (85.55%), Space: 0B (100.00%) - LeetHub

---
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT employee_id, count AS team_size
+FROM Employee
+LEFT JOIN (
+    SELECT team_id, COUNT(*) as count
+    FROM Employee
+    GROUP BY team_id
+) Team
+ON Team.team_id = Employee.team_id
