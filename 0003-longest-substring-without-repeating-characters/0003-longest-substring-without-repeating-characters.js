47b324e1b6a2d35c3c510c725d7ef53fa49265b8
Daniel Yoo <dacapo0509@gmail.com>
2023-07-27 13:32:08 -0700

    Time: 63 ms (99.42%), Space: 46.5 MB (76.91%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,16 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let res = 0;
-    let mp = {};
-
-    for (let j = 0, i = 0; j < s.length; j++) {
-      if (mp[s[j]] > 0) {
-        i = Math.max(mp[s[j]], i);
-      }
-      res = Math.max(res, j - i + 1);
-      mp[s[j]] = j + 1;
+    const set = new Set();
+    let l = 0;
+    let max = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        while(set.has(s[i])){
+            set.delete(s[l])
+            l++;
+        }
+        set.add(s[i])
+        max= Math.max(max,set.size)
     }
-    return res;
+    return max
   
 };
