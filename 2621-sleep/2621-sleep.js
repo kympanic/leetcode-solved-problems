369cb11f6a74ffb8aeb7cc49e7b48b92b7ba535d
Daniel Yoo <dacapo0509@gmail.com>
2024-01-08 16:45:39 -0800

    Time: 53 ms (56.92%), Space: 41.1 MB (94.08%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+  return new Promise((res,rej) => {
+    try {
+      setTimeout(() => res(5), millis)
+    } catch(err) {
+      rej(err)
+    }
+  })
+}
