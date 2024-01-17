0f7e44d0582a71181151321ef47a66e76d6e2556
Daniel Yoo <dacapo0509@gmail.com>
2024-01-16 20:08:44 -0800

    Time: 70 ms (28.23%), Space: 43.8 MB (68.90%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  const absReversed = Math.abs(x).toString().split('').reverse().join('');
+  if (absReversed > 2**31) return 0;
+  return absReversed * Math.sign(x);
+};
