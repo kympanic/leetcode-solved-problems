261d873ac7caf8eec2adede007374afcba589136
Daniel Yoo <dacapo0509@gmail.com>
2024-01-16 20:10:26 -0800

    Time: 45 ms (90.12%), Space: 42 MB (66.31%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let i = 0;
+    while (nums[i] < target) { i++ };
+    return i  
+};
