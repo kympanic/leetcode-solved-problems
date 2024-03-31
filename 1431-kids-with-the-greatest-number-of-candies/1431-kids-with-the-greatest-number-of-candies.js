b1a4bde3cd7436e918405ab06ad55bc19a6e3b35
Daniel Yoo <dacapo0509@gmail.com>
2024-03-31 13:11:30 -0700

    Time: 50 ms (90.47%), Space: 49.9 MB (88.10%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies);
+    return candies.map(item=>item + extraCandies )
+};
+
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = Math.max(...candies);
+    return candies.map(item => item + extraCandies >= max);
+};
