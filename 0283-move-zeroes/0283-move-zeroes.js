2f3a38a09ded1b8c65e5aca8e6624317f718fde4
Daniel Yoo <dacapo0509@gmail.com>
2024-04-03 11:14:44 -0700

    Time: 71 ms (79.37%), Space: 53.7 MB (77.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,14 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-     let left = 0 ;
+  let left = 0;
     for(let num in nums){
         if(nums[num] !== 0){
-            let temp = nums[num];
-            nums[num] = nums[left];
+            let temp = nums[num]
+            nums[num] = nums[left]
             nums[left] = temp;
-            left++;
+            left++
         }
     }
-    return nums;
+    return nums
 };
+
