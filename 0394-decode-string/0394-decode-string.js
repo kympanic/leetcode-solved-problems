b253fbccdd4a2ffe37963553a7cabf4d966975fd
Daniel Yoo <dacapo0509@gmail.com>
2024-04-20 21:38:59 -0700

    Time: 55 ms (34.39%), Space: 48.4 MB (87.82%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+	const stack = []
+
+	for(let i=0; i< s.length; i++){
+		if(s[i] !== ']'){
+			stack.push(s[i]) 
+		}else{
+			let substr = ''
+			while(stack[stack.length-1] !== '['){ // go up until '['
+				substr = stack.pop() + substr
+			}
+			stack.pop()  // remove '['
+
+			let k =''
+			while(stack && !isNaN(stack[stack.length-1])){ // check if it's a number
+				k = stack.pop() + k
+			}
+			stack.push(substr.repeat(parseInt(k)))  // repeat the string as many times as k
+		}
+	}
+	return stack.join('')
+};
