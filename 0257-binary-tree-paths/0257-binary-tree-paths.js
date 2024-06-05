ea73c53e3b30f1e9c6b1804b249b5023f76dc840
Daniel Yoo <dacapo0509@gmail.com>
2024-06-05 12:42:03 -0700

    Time: 61 ms (37.80%), Space: 51.3 MB (21.05%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    let result = [];
+    traverse(root, "");
+    
+    function traverse(node, path) {
+        if (!node) return;
+ 
+        if (!node.left  && !node.right) {
+            result.push(path + node.val);
+            return;
+        }
+        traverse(node.left, path + node.val + "->");
+        traverse(node.right, path + node.val + "->");
+    }
+    return result;
+};
